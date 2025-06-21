790c5f44043ddd31ce4b4aa71db5116eede3cc98
bertan-arslan <bertanars97@gmail.com>
2025-06-21 23:35:05 +0300

    login and success pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,17 @@
-import React, {useState} from 'react'
-import { Button, Form, FormGroup, Label, Input, Card, CardTitle, CardBody } from 'reactstrap';
-import './App.css'
-
-const formInitial = {
-  email: "",
-  password: "",
-  terms: false
-}
+import "./App.css"
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Success from './components/Success';
 
 function App() {
-
-  const [formData, setFormData] = useState(formInitial)
-
-  const handleChange = (event) => {
-    const {name, value} = event.target;
-    setFormData({...formData, [name]: value})
-  }
-
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  }
-  
-  return (
-    <Card>
-      <CardTitle tag="h5">
-      Kayıt ol
-      </CardTitle>
-      <CardBody>
-        <Form onSubmit={handleSubmit}>
-          <FormGroup>
-            <Label for="email">
-              Eposta:
-            </Label>
-            <Input
-              id="email"
-              name="email"
-              placeholder="Eposta adresinizi giriniz"
-              type="email"
-              onChange = {handleChange}
-              value={formData.email}
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="password">
-              Şifre:
-            </Label>
-            <Input
-              id="password"
-              name="password"
-              placeholder="Güçlü bir şifre seçiniz"
-              type="password"
-              onChange = {handleChange}
-              value={formData.password}
-            />
-          </FormGroup>
-          <FormGroup check>
-            <Input 
-              type="checkbox"
-              id="password"
-              name="password"
-              onChange = {handleChange}
-              checked={formData.terms}
-            />
-            <Label check for="terms">
-              Kullanım koşullarını, gizlilik politikasını ve KVKK kapsamında kişisel verilerimin işlenmesini kabul ediyorum.
-            </Label>
-          </FormGroup>
-          <Button>
-          Kayıt ol
-          </Button>
-        </Form>
-      </CardBody>
-    </Card>
-  )
+  return(
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route path="/success" component={Success} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App
---
+++ src/components/Success.jsx
@@ -0,0 +1,17 @@
+
+
+export default function Success() {
+    return (
+        <Card>
+            <img  src="https://smb.ibsrv.net/imageresizer/image/blog_images/1200x1200/10087/91160/0447463001544828845.jpg" />
+            <CardBody>
+                <CardTitle tag="h5">
+                Tebrikler!
+                </CardTitle>
+                <CardText>
+                Kayıt işleminiz başarıyla tamamlanmıştır
+                </CardText>
+            </CardBody>
+        </Card>
+    )
+}
